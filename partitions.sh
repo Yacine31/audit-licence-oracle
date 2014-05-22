934978de2479e5a235c13171836349241d47116b
yacine <yacine.oumghar@gmail.com>
2014-05-22 16:52:05 +0200

    insertion des données des fichiers verions.csv ds la base de données

--- partitions.sh
+++ partitions.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 :<<HISTORIQUE
 20/05/2014 - insertion des données sur le partitioning dans la base MySQL
+22/05/2014 - insersion des données des fichiers versions.csv ds la base
 HISTORIQUE
 
 # si aucun paramètre en entrée on quitte
@@ -16,7 +17,7 @@ function fnCreateTable {
 	CSV=$2
 
 	# une fois qu'on a le fichier CSV, on récupère la 4ème ligne qui contient les entetes
-	FIELDS=$(grep "^AUDIT_ID," "$CSV" | sed 's/ /_/g' | sed -e 's/'$DELIM'/` varchar(255),\n`/g' -e 's/\r//g')
+	FIELDS=$(grep "^AUDIT_ID," "$CSV" | head -1 | sed 's/ /_/g' | sed -e 's/'$DELIM'/` varchar(255),\n`/g' -e 's/\r//g')
 	FIELDS='`'"$FIELDS"'` varchar(255)'
 	echo -n "Création de la table " $TABLE " .... "
 	mysql -uroot -proot --local-infile --database=$DB -e "
@@ -77,5 +78,31 @@ function fnAdminPack {
 	echo ""
 }
 
+function fnVersion {
+	# insertion des données sur les versions
+	# les données proviennent des fichiers version.csv
+	SRCFILE="*_version.csv"
+	TABLE=$1"_version"
+	TMPFILE="/tmp/version.csv"
+
+	# creation de la table à partir d'un fichier XXX_YYY_version.csv
+	# onprend le premier fichier qu'on trouve pour cela :
+	CSV=$(find -type f -iname $SRCFILE | head -1)
+	# on appel la fonction pour créer la table
+	fnCreateTable $TABLE $CSV
+
+	echo -n "Insertion des fichiers XXX_YYY_version.csv dans la table $TABLE : "
+	find -type f -iname $SRCFILE | while read f
+	do 
+		echo -n "."
+		cat $f | grep "^0," | sed 's/"//g' > $TMPFILE
+		mysql -uroot -proot --local-infile --database=$DB -e "
+		load data local infile '$TMPFILE' into table $TABLE fields terminated by '$DELIM' ;"
+		rm -f $TMPFILE
+	done
+	echo ""
+}
+
 fnPart $1
 fnAdminPack $1
+fnVersion $1
